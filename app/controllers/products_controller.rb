1ffbdc726ab74f86aa746f5018ea0305bace6f46
Antonio Terceiro <terceiro@colivre.coop.br>
2006-12-07 01:29:10 +0000

    r21@fork:  terceiro | 2006-12-06 21:15:29 -0300  adding an almost-scaffold controller for products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,55 @@
+class ProductsController < ApplicationController
+
+  needs_organization
+
+  def index
+    list
+    render :action => 'list'
+  end
+
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+
+  def list
+    @products = @organization.products
+  end
+
+  def show
+    @product = @organization.products.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    @product.organization = @organization
+    if @product.save
+      flash[:notice] = 'Product was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @product = @organization.products.find(params[:id])
+  end
+
+  def update
+    @product = @organization.products.find(params[:id])
+    if @product.update_attributes(params[:product])
+      flash[:notice] = 'Product was successfully updated.'
+      redirect_to :action => 'show', :id => @product
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @organization.products.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
+end
---
+++ test/functional/products_controller_test.rb
@@ -0,0 +1,92 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'products_controller'
+
+# Re-raise errors caught by the controller.
+class ProductsController; def rescue_action(e) raise e end; end
+
+class ProductsControllerTest < Test::Unit::TestCase
+
+  include TestingUnderOrganization
+
+  fixtures :products
+
+  def setup
+    @controller = ProductsController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    @organization_nickname = 'one'
+  end
+
+  def test_index
+    get :index
+    assert_response :success
+    assert_template 'list'
+  end
+
+  def test_list
+    get :list
+
+    assert_response :success
+    assert_template 'list'
+
+    assert_not_nil assigns(:products)
+  end
+
+  def test_show
+    get :show, :id => 1
+
+    assert_response :success
+    assert_template 'show'
+
+    assert_not_nil assigns(:product)
+    assert assigns(:product).valid?
+  end
+
+  def test_new
+    get :new
+
+    assert_response :success
+    assert_template 'new'
+
+    assert_not_nil assigns(:product)
+  end
+
+  def test_create
+    num_products = Product.count
+
+    post :create, :product => { :description => 'Testing adding a product through controller', :sell_price => 99.99 }
+
+    assert_response :redirect
+    assert_redirected_to :action => 'list'
+
+    assert_equal num_products + 1, Product.count
+  end
+
+  def test_edit
+    get :edit, :id => 1
+
+    assert_response :success
+    assert_template 'edit'
+
+    assert_not_nil assigns(:product)
+    assert assigns(:product).valid?
+  end
+
+  def test_update
+    post :update, :id => 1
+    assert_response :redirect
+    assert_redirected_to :action => 'show', :id => 1
+  end
+
+  def test_destroy
+    assert_not_nil Product.find(1)
+
+    post :destroy, :id => 1
+    assert_response :redirect
+    assert_redirected_to :action => 'list'
+
+    assert_raise(ActiveRecord::RecordNotFound) {
+      Product.find(1)
+    }
+  end
+end

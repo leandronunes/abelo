30e4f164ee95b3a180f23793806806d348a3077d
Antonio Terceiro <terceiro@colivre.coop.br>
2006-12-07 01:30:49 +0000

    r25@fork:  terceiro | 2006-12-06 22:07:44 -0300  committing random stuff

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
 
   layout 'admin'
 
+  def self.needs_organization
+    before_filter :load_organization
+  end
+  def load_organization
+    @organization = Organization.find_by_nickname(params[:organization_nickname])
+    render :text => _('There is no organization with nickname %s') % params[:organization_nickname] unless @organization
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+
+  def button(title, type, url_options = {}, html_options = {})
+    local_html_options = html_options.merge({ :class => "button button_#{type}" })
+    if [ :save ].include? type 
+      submit_tag(title, local_html_options)
+    else
+      link_to(title, url_options, local_html_options)
+    end
+  end
+
 end

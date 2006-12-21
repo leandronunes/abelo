8632fc8975057f57b0331df34f9c8946f1b0cc34
Antonio Terceiro <terceiro@colivre.coop.br>
2006-12-21 23:33:08 +0000

    r56@fork:  terceiro | 2006-12-21 17:34:11 -0300  picking colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,24 @@ module ApplicationHelper
     end
   end
 
+  def color_select(object, method)
+    colors = []
+    (0..15).step(3) do |r|
+      (0..15).step(3) do |g|
+        (0..15).step(3) do |b|
+          colors << '#' + r.to_s(16) + g.to_s(16) + b.to_s(16)
+        end
+      end
+    end
+    value = self.instance_variable_get("@#{object}").send(method)
+    select_tag(
+      "#{object}[#{method}]",
+      colors.map { |c|
+        selected = (c==value) ? 'selected' : ''
+        "<option value=\"#{c}\" style=\"background: #{c}\" #{selected}>#{c}</option>" 
+      }.join("\n"),
+      :style => "background: #{value}", :id => "#{object}_#{method}"
+    ) + observe_field("#{object}_#{method}", :function => "element.style.background = value" )
+  end
+
 end

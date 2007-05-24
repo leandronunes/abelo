4b232505d5b1012b583142a27048ed7f1aebb396
keilla <keilla@colivre.coop.br>
2007-05-24 16:56:17 +0000

    add organization_id in historicals and a new migrate

--- db/migrate/022_create_historicals.rb
+++ db/migrate/022_create_historicals.rb
@@ -1,9 +1,10 @@
 class CreateHistoricals < ActiveRecord::Migration
   def self.up
     create_table :historicals do |t|
-      t.column :name,         :string,              :null => false
-      t.column :type_of,      :string, :limit => 1, :null => false
-      t.column :operational,  :boolean,             :null => false
+      t.column :name,               :string,              :null => false
+      t.column :type_of,            :string, :limit => 1, :null => false
+      t.column :operational,        :boolean,             :null => false
+      t.column :organization_id,    :int,                 :null => false
     end
   end
 

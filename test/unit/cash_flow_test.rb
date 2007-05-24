55faa2fd91b413a5732e9aa3549675ad4292eb93
keilla <keilla@colivre.coop.br>
2007-05-24 16:52:56 +0000

    cash_flows test

--- test/unit/cash_flow_test.rb
+++ test/unit/cash_flow_test.rb
@@ -3,8 +3,22 @@ require File.dirname(__FILE__) + '/../test_helper'
 class CashFlowTest < Test::Unit::TestCase
   fixtures :cash_flows
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_belongs_to_historical
+    cf = CashFlow.find(1)
+    assert_valid cf
+    h = Historical.find(1)
+    assert_valid h
+    cf.historical = h
+    assert_equal cf.historical, h
   end
+
+  def test_belongs_to_organization
+    cf = CashFlow.find(1)
+    assert_valid cf
+    o = Organization.find(1)
+    assert_valid o
+    cf.organization = o
+    assert_equal cf.organization, o
+  end
+
 end

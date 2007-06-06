939e56d83f92ffff900fd54fc62301db7732a906
Livia Barreiros <livia@colivre.coop.br>
2007-06-06 17:17:55 +0000

    adjustments for the full extract per year

--- app/helpers/cash_flows_helper.rb
+++ app/helpers/cash_flows_helper.rb
@@ -1,6 +1,7 @@
 module CashFlowsHelper
 
   def generate_extract(extract, period, historicals, cash_flows)
+    cash_flows = cash_flows.sort {|x, y| x.date <=> y.date }
     if historicals.empty?
       content_tag('p', _('None')) 
     else
@@ -48,16 +49,27 @@ module CashFlowsHelper
               end
             }
           )
-	elsif period == "year" 
+	elsif period == "year"  
           content_tag('table',
             {  
               content_tag('tr', { content_tag('th', _('Mês') ), content_tag('th', _('Value') )}),
               historicals.map do |h|
                 {content_tag('tr', content_tag('th', h.name, "colspan" => "2")),
                 cash_flows.map do |c|
+		  month = c.date.month
+		  month_value = 0.0
+		  i = 0
+		  while i <= cash_flows.length-1
+		    if (month == cash_flows[i].date.month)
+		      month_value += cash_flows[i].value
+		      cash_flows.delete_at(i)
+		      i = i - 1
+		    end
+		    i = i + 1
+		  end
   	          if c.historical_id == h.id	  
                       content_tag('tr',
-                        { content_tag('td', c.date.month), content_tag('td', c.value ) }
+                        { content_tag('td', month), content_tag('td', month_value ) }
   		    )
   		  end
   	        end}

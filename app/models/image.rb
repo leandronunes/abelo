7054be88192c7fec10578e3b6fca55544d932a04
keilla <keilla@colivre.coop.br>
2007-08-01 13:27:17 +0000

    unit tests

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,7 @@
 class Image < ActiveRecord::Base
 
-  validates_presence_of :description, :picture
-
   belongs_to :product
-
+  validates_presence_of :description, :picture
   file_column :picture, :magick => {
     :versions => { :thumb => '120x120', :medium => '320x240' }
   }
--- test/unit/image_test.rb
+++ test/unit/image_test.rb
@@ -3,13 +3,13 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ImageTest < Test::Unit::TestCase
   fixtures :images, :products
 
-  def test_mandatory_fields
+  def setup
+    @org = Organization.create(:name => 'Organization for testing', :cnpj => '63182452000151', :nickname => 'org')
+  end
+
+  def test_mandatory_field_description
     img = Image.new
-    assert(!img.save)
-
-    img.description = 'a image'
-    assert(!img.save)
-
+    
     stream = StringIO.new(File.read('public/images/rails.png'))
     def stream.original_filename
       'rails.png'
@@ -18,13 +18,26 @@ class ImageTest < Test::Unit::TestCase
       'image/png'
     end
 
-    img.product = Product.find(1)
-
     img.picture = stream
-    assert(img.save)
-
+    img.save
+    
+    assert img.errors.invalid?(:description)
     assert_equal img[:picture], 'rails.png'
     assert_equal "#{img.id}/rails.png", img.picture_relative_path
-
   end
+
+  def test_mandatory_field_picture
+    img = Image.new
+    img.description = 'Image for testing'
+    img.save
+    assert img.errors.invalid?(:picture)
+  end
+
+  def test_relation_with_product
+    img = Image.new
+    product = Product.create(:name => 'Image of product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id) 
+    img.product = product
+    assert_equal product, img.product
+  end
+
 end

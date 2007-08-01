5e25bd99163eb87c4f97e7daff1a38b675275872
keilla <keilla@colivre.coop.br>
2007-08-01 13:36:32 +0000

    unit test

--- app/models/mass_mail.rb
+++ app/models/mass_mail.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-# Schema version: 35
-#
-# Table name: mass_mails
-#
-#  id              :integer       not null, primary key
-#  subject         :string(255)   not null
-#  body            :text          not null
-#  organization_id :integer       not null
-#
-
 class MassMail < ActiveRecord::Base
 
   belongs_to :organization

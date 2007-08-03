7f8052a12b27bb3c6a8e24c61aa0a2eb686f6047
keilla <keilla@colivre.coop.br>
2007-08-03 17:20:40 +0000

    unit tests

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,19 +1,8 @@
-# == Schema Information
-# Schema version: 35
-#
-# Table name: categories
-#
-#  id              :integer       not null, primary key
-#  name            :string(255)   not null
-#  type            :string(255)   not null
-#  parent_id       :integer       
-#  organization_id :integer       not null
-#
-
 class ProductCategory < Category
 
   has_many :products, :foreign_key => 'category_id'
   has_many :images, :through => :products
+
   validates_uniqueness_of :name, :scope => [:organization_id], :message => _('The name %{fn} for a product category was already taken.')
 
 #TODO see what is the use of this
--- test/unit/product_category_test.rb
+++ test/unit/product_category_test.rb
@@ -1,52 +1,64 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ProductCategoryTest < Test::Unit::TestCase
-  fixtures :product_categories, :organizations
 
-  # Replace this with your real tests.
-  def test_mandatory_fields
-    count = ProductCategory.count
-
-    cat = ProductCategory.new
-    assert !cat.save
-
-    cat.name = 'A category for testing mandatory fields'
-    assert !cat.save
-
-    cat.organization = Organization.find(1)
-    assert cat.save
-
-    assert_equal count + 1, ProductCategory.count
+  def setup
+    @org = Organization.create(:name => 'Organization for testing', :cnpj => '63182452000151', :nickname => 'org')
   end
 
-  def test_full_name
-    c = ProductCategory.find(1)
-    assert_equal 'Category A', c.full_name
-    c = ProductCategory.find(3)
-    assert_equal 'Category A/Category AA', c.full_name
-    assert_equal 'Category A--Category AA', c.full_name('--')
+  def test_relation_with_products
+    cat_prod = ProductCategory.create(:name => 'Category for testing', :organization_id => @org.id)
+    product = Product.new(:name => 'Image of product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id)
+    cat_prod.products.concat(product)
+    assert_equal 1, cat_prod.products.count
   end
 
-  def test_leaf
-    c1 = ProductCategory.find(1)
-    assert !c1.leaf?
+  def test_relation_with_images
+    cat_prod = ProductCategory.create(:name => 'Category for testing', :organization_id => @org.id)
+    product = Product.create(:name => 'Image of product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => cat_prod.id)
+    img = Image.new 
+    stream = StringIO.new(File.read('public/images/rails.png'))
+    def stream.original_filename
+      'rails.png'
+    end
+    def stream.content_type
+      'image/png'
+    end
+    img.description = ('Image for testing')
+    img.picture = stream
+    img.product = product
+    img.save
 
-    c2 = ProductCategory.find(2)
-    assert c2.leaf?
+    assert cat_prod.images.include?(img)
   end
 
-  def test_uniqueness
-    category_a = Category.find(1)
+  def test_uniqueness_name
+    cat_prod_1 = ProductCategory.create(:name => 'Category for testing', :organization_id => @org.id)
+    cat_prod_2 = ProductCategory.create(:name => 'Category for testing', :organization_id => @org.id)
+    assert cat_prod_2.errors.invalid?(:name)
+  end
 
-    category_b = ProductCategory.new
-    category_b.name = category_a.name
-    category_b.organization = category_a.organization
-    assert !category_b.save
+  def test_category_images
+    cat_prod_1 = ProductCategory.create(:name => 'Category for testing', :organization_id => @org.id)
+    cat_prod_2 = ProductCategory.create(:name => 'Category for testing 2', :organization_id => @org.id, :parent_id => cat_prod_1.id)
+    product = Product.create(:name => 'Image of product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => cat_prod_2.id)
 
-    category_b = SupplierCategory.new
-    category_b.name = category_a.name
-    category_b.organization = category_a.organization
-    assert category_b.save
+    img = Image.new 
+    stream = StringIO.new(File.read('public/images/rails.png'))
+    def stream.original_filename
+      'rails.png'
+    end
+    def stream.content_type
+      'image/png'
+    end
+    img.description = ('Image for testing')
+    img.picture = stream
+    img.product = product
+    img.save
+
+    assert cat_prod_1.category_images.include?(img)
+    assert cat_prod_2.category_images.include?(img)
+
   end
 
 end

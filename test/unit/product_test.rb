b4eacd82739037713136822e873bb4bc2298b6ae
keilla <keilla@colivre.coop.br>
2007-08-08 15:22:23 +0000

    unit test

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -14,12 +14,12 @@ class ProductTest < Test::Unit::TestCase
     assert_equal @org, product.organization
   end
 
-  def test_relation_with_organization
+  def test_relation_with_category
     product = Product.create(:name => 'product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => @cat_prod.id)
     assert_equal @cat_prod, product.category
   end
 
-  def test_relation_with_organization
+  def test_relation_with_images
     product = Product.create(:name => 'product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => @cat_prod.id)
     img = Image.new 
     stream = StringIO.new(File.read('public/images/rails.png'))
@@ -49,29 +49,6 @@ class ProductTest < Test::Unit::TestCase
     assert product.stock_entries.include?(entry)
   end
 
-  def test_full_text_search
-    product = Product.create(:name => 'product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => @cat_prod.id)
-    products = Product.full_text_search('prod*')
-    assert_equal 1, products[0]
-    assert products[1].include?(product)
-  end
-
-  def test_amount_in_stock
-    product = Product.create(:name => 'product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => @cat_prod.id)
-
-    # generate 10 stock ins
-    total_ammount = 0.0
-    total_cost = 0.0
-    (1..10).each { |n|
-      entry = StockIn.create!(:supplier_id => @supplier.id, :ammount => 5, :price => '1.00', :purpose => 'sell', :date => '2007-07-01', :payment_status => true, :product_id => product.id)
-      total_ammount += entry.ammount
-      total_cost += entry.price * entry.ammount
-    }
-
-    assert_equal total_ammount, product.ammount_in_stock
-    assert_in_delta total_cost, product.total_cost, 0.01
-  end
-  
   def test_mandatory_field_name
     product = Product.create(:sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => @cat_prod.id)
     assert product.errors.invalid?(:name)
@@ -97,4 +74,27 @@ class ProductTest < Test::Unit::TestCase
     assert product.errors.invalid?(:category_id)
   end
 
+  def test_full_text_search
+    product = Product.create(:name => 'product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => @cat_prod.id)
+    products = Product.full_text_search('prod*')
+    assert_equal 1, products[0]
+    assert products[1].include?(product)
+  end
+
+  def test_amount_in_stock
+    product = Product.create(:name => 'product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => @cat_prod.id)
+
+    # generate 10 stock ins
+    total_ammount = 0.0
+    total_cost = 0.0
+    (1..10).each { |n|
+      entry = StockIn.create!(:supplier_id => @supplier.id, :ammount => 5, :price => '1.00', :purpose => 'sell', :date => '2007-07-01', :payment_status => true, :product_id => product.id)
+      total_ammount += entry.ammount
+      total_cost += entry.price * entry.ammount
+    }
+
+    assert_equal total_ammount, product.ammount_in_stock
+    assert_in_delta total_cost, product.total_cost, 0.01
+  end
+  
 end

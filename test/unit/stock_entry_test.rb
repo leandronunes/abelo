37d37c2a03a877ea8f3879b42065182b7175e0bd
keilla <keilla@colivre.coop.br>
2007-08-08 19:46:05 +0000

    unit tests

--- test/unit/stock_entry_test.rb
+++ test/unit/stock_entry_test.rb
@@ -1,12 +1,25 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class StockEntryTest < Test::Unit::TestCase
-  fixtures :stock_entries, :products
+
+  def setup
+    @org = Organization.create(:name => 'Organization for testing', :cnpj => '63182452000151', :nickname => 'org')
+    @cat_prod = ProductCategory.create(:name => 'Category for testing', :organization_id => @org.id)
+    @product = Product.create(:name => 'product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => @cat_prod.id) 
+    cat_supp = SupplierCategory.create(:name => 'Category for testing', :organization_id => @org.id)
+    @supplier = Supplier.create!(:name => 'Hering', :cnpj => '58178734000145', :organization_id => @org.id, :email => 'contato@hering.com', :category_id => cat_supp.id)
+  end
 
   def test_relation_with_product
-    entry = StockEntry.find(1)
-    assert_not_nil entry.product
-    assert_equal Product, entry.product.class
+    entry = StockEntry.new
+    entry.product = @product
+    assert_equal @product, entry.product
+  end
+
+  def test_relation_with_organization
+    entry = StockIn.create!(:supplier_id => @supplier.id, :ammount => 5, :price => '1.00', :purpose => 'sell', :date => '2007-07-01', :payment_status => true, :product_id => @product.id)
+    entry.organization = @org
+    assert_equal @org, entry.organization
   end
 
   def test_belongs_organization

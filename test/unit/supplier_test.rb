dc59a58ee1b965acf4c0c910f759f9b0b96fb065
keilla <keilla@colivre.coop.br>
2007-08-10 15:05:05 +0000

    unit test

--- test/unit/supplier_test.rb
+++ test/unit/supplier_test.rb
@@ -1,106 +1,24 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class SupplierTest < Test::Unit::TestCase
-  fixtures :suppliers
 
-  # Replace this with your real tests.
-  def test_mandatory_fields
-
-    count = Supplier.count
-
-    s = Supplier.new
-    assert !s.save
-
-    s.name = 'A test supplier'
-    assert !s.save
-
-    s.cnpj = '47635123000162'
-    assert !s.save
-
-    s.organization_id = 1
-    assert s.save
-
-    assert_equal count + 1, Supplier.count
-
+  def setup
+    @org = Organization.create(:name => 'Organization for testing', :cnpj => '63182452000151', :nickname => 'org')
   end
 
-  def test_cnpj_format
-    count = Supplier.count
-    s = Supplier.new(:name => 'A supplier for testing CNPJ format', :organization_id => 1)
-    s.cnpj = '00000000000000'
-    assert !s.save
-    s.cnpj = '85978357000100'
-    assert s.save
-    assert_equal count + 1, Supplier.count
+  def test_relation_with_products
+    cat_prod = ProductCategory.create(:name => 'Category for testing', :organization_id => @org.id)
+    product = Product.create(:name => 'product', :sell_price => 2.0, :unit => 'kg', :organization_id => @org.id, :category_id => cat_prod.id)
+    supplier = Supplier.new
+    supplier.products.concat(product)
+    assert supplier.products.include?(product)
   end
 
-  def test_products
-    s = Supplier.find(1)
-    assert_not_nil s.products
-    assert_equal Array, s.products.class
-    s.products.each { |p|
-      assert_equal Product, p.class
-    }
-  end
-
-  def test_cnpj_or_cpf
-
-    s = Supplier.find(1)
-    s.cpf = '27774667857'
-    assert !s.save
-    s.cnpj = nil
-    assert s.save
-
-    s = Supplier.find(3)
-    s.cnpj = '28695988000170'
-    assert !s.save
-    s.cnpj = nil
-    assert s.save
-
-  end
-
-  def test_cnpj_uniq
-    count = Supplier.count
-    s1 = Supplier.new(:name => 'Testing unique CNPJ (first)', :organization_id => 1)
-    s1.cnpj = '22071350000181'
-    assert s1.save
-    assert_equal count + 1, Supplier.count
-
-    # the same organization cannot have the same supplier registered twice
-    count = Supplier.count
-    s2 = Supplier.new(:name => 'Testing unique CNPJ (second)', :organization_id => 1)
-    s2.cnpj = '22071350000181'
-    assert !s2.save
-    assert_equal count, Supplier.count
-
-    # another organization can have the same supplier registered
-    count = Supplier.count
-    s3 = Supplier.new(:name => 'Testing unique CNPJ (another organization)', :organization_id => 2)
-    s3.cnpj = '22071350000181'
-    assert s3.save
-    assert_equal count + 1, Supplier.count
-  end
-
-  def test_cpf_uniq
-    count = Supplier.count
-    s1 = Supplier.new(:name => 'Testing unique CPF (first)', :organization_id => 1)
-    s1.cpf = '18385961437'
-    assert s1.save
-    assert_equal count + 1, Supplier.count
-
-    # the same organization cannot have the same supplier registered twice
-    count = Supplier.count
-    s2 = Supplier.new(:name => 'Testing unique CPF (second)', :organization_id => 1)
-    s2.cpf = '18385961437'
-    assert !s2.save
-    assert_equal count, Supplier.count
-
-    # another organization can have the same supplier registered
-    count = Supplier.count
-    s3 = Supplier.new(:name => 'Testing unique CPF (another organization)', :organization_id => 2)
-    s3.cpf = '18385961437'
-    assert s3.save
-    assert_equal count + 1, Supplier.count
+  def test_relation_with_category
+    cat_supp = SupplierCategory.create(:name => 'Category for testing', :organization_id => @org.id)
+    supplier = Supplier.new
+    supplier.category = cat_supp
+    assert_equal cat_supp, supplier.category
   end
 
 end

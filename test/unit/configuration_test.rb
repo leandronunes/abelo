a84ac9893a13ac861be53cb8cc525ea650863fd6
Leandro Nunes <leandronunes@gmail.com>
2007-08-17 20:41:48 +0000

    Fixing bug in configurations test

--- test/unit/configuration_test.rb
+++ test/unit/configuration_test.rb
@@ -5,34 +5,37 @@ class ConfigurationTest < Test::Unit::TestCase
 
   def test_full_product_is_array_when_instanciate
     c = Configuration.new 
-    assert_kind_of c.full_product, Array
+    assert_kind_of Array, c.full_product
+  end
+
+  def test_lite_product_is_array_when_instanciate
+    c = Configuration.new 
+    assert_kind_of Array, c.lite_product
   end
 
   def test_full_product_is_array_after_load
     o = Organization.find(:first)
     c = Configuration.new
-    c.owner = o
-    c.Organization = o
+    c.organization = o
     c.full_product = ["size", "name"]
     assert c.save
     c2 = Configuration.find(c.id) 
-    assert_kind_of c2.full_product, Array
+    assert_kind_of Array, c2.full_product
   end
 
   def test_lite_product_is_array_after_load
     c = Configuration.new 
-    assert_kind_of c.lite_product, Array
+    assert_kind_of Array, c.lite_product
   end
 
   def test_lite_product_is_array_after_load
     o = Organization.find(:first)
     c = Configuration.new
-    c.owner = o
-    c.Organization = o
     c.lite_product = ["size", "name"]
+    c.organization = o
     assert c.save
     c2 = Configuration.find(c.id) 
-    assert_kind_of c2.lite_product, Array
+    assert_kind_of Array, c2.lite_product
   end
 
 end

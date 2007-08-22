ceadab9b5ac18aedcfc478957f7cc6b19bd4d665
Helder Santana <helder@colivre.coop.br>
2007-08-22 19:16:05 +0000

    Define configuration.

--- db/migrate/032_create_configurations.rb
+++ db/migrate/032_create_configurations.rb
@@ -9,6 +9,10 @@ class CreateConfigurations < ActiveRecord::Migration
       t.column :lite_product,       :text
       t.column :full_customer,      :text
       t.column :lite_customer,      :text
+      t.column :full_worker,        :text
+      t.column :lite_worker,        :text
+      t.column :full_supplier,      :text
+      t.column :lite_supplier,      :text
       t.column :department_name,    :string
       t.column :product_name,       :string
       t.column :customer_name,      :string

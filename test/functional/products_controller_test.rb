2e3b3c1b3af60c8d5c35899bf9c1e5f9925e9c1c
keilla <keilla@colivre.coop.br>
2007-09-05 14:00:24 +0000

    ActionItem65: change autocomplete test replacing id's for objects

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -16,6 +16,7 @@ class ProductsControllerTest < Test::Unit::TestCase
     @response   = ActionController::TestResponse.new
     @organization_nickname = 'one'
     @organization = Organization.find_by_nickname 'one'
+    @category = ProductCategory.find(1)
     login_as("quentin")
   end
 
@@ -37,7 +38,7 @@ class ProductsControllerTest < Test::Unit::TestCase
 
   def test_autocomplete_name
     Product.delete_all
-    product = Product.create!(:name => 'test product', :sell_price => 2.0, :unit => 'kg', :organization_id => 1, :category_id => 1)
+    product = Product.create!(:name => 'test product', :sell_price => 2.0, :unit => 'kg', :organization => @organization, :category => @category)
     get :autocomplete_name, :product => { :name => 'test'}
     assert_not_nil assigns(:products)
     assert_kind_of Array, assigns(:products)

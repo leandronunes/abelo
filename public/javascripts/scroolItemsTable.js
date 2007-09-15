5f6842594f77e4edb5cb9a97be8eabf477be3287
Aurelio A. Heckert <aurelio@colivre.coop.br>
2007-09-15 22:23:07 +0000

    ActionItem85: done. Também dei um trato no ponto de venda que foi bastante modificado.

--- public/javascripts/scroolItemsTable.js
+++ public/javascripts/scroolItemsTable.js
@@ -1,7 +1,7 @@
 <!-- FIXME tratar este java script para verificar se existe a div antes de tratar o html-->
 function scroolItemsTable() {
   var div = $("cumpom_sales");
-  if ( ( div.scrollTop + div.clientHeight ) < div.scrollHeight ) {
+  if ( div && ( div.scrollTop + div.clientHeight ) < div.scrollHeight ) {
     div.scrollTop++;
     setTimeout("scroolItemsTable()",50);
   }

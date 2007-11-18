f126b9f0f86d8dee96918f1b18889acb322619a1
Leandro Nunes <leandronunes@gmail.com>
2007-11-18 20:27:08 +0000

    ActionItem65: set enviroment to production

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Uncomment below to force Rails into production mode when 
 # you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
+ENV['RAILS_ENV'] ||= 'production'
 
 APP_DEMO = false
 

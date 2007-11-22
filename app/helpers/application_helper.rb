9c66363a8aaa8731ab91eccfaa389902627198af
Leandro Nunes <leandronunes@gmail.com>
2007-11-22 20:25:39 +0000

    ActionItem65: updating systema

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,8 +39,8 @@ module ApplicationHelper
 
   def show_image(image, size = "50x50")
     link_to(
-      content_tag('span', image_tag(url_for_image_column(image, 'picture', :name => 'thumb'), :size => size)),
-      url_for_image_column(image, 'picture', :name => 'medium'),
+      content_tag('span', image_tag(url_for_file_column(image, 'picture', :name => 'thumb'), :size => size)),
+      url_for_file_column(image, 'picture', :name => 'medium'),
       { :class => 'product_image', :popup => [ 'abelo_image', 'height=240,width=320' ] }
     )
   end

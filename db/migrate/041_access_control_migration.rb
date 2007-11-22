9bf5cb00b8651338a1255bd8dfaaefcb314d1444
Orivaldo Vieira <orivaldo@colivre.coop.br>
2007-11-22 19:13:40 +0000

    ActionItem65: Add column is_global for access control migration

--- db/migrate/041_access_control_migration.rb
+++ db/migrate/041_access_control_migration.rb
@@ -11,6 +11,8 @@ class AccessControlMigration < ActiveRecord::Migration
       t.column :resource_id,   :integer
       t.column :resource_type, :string
       t.column :role_id,       :integer
+      t.column :is_global,     :boolean
+
     end
   end
 

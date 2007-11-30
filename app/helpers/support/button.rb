66af2aeccf116a0698c6b5cf37dc17dae1f6037f
Leandro Nunes <leandronunes@gmail.com>
2007-11-30 20:24:53 +0000

    ActionItem65: making pog

--- app/helpers/support/button.rb
+++ app/helpers/support/button.rb
@@ -1,7 +1,7 @@
 
 
   def button(button, title, type, url_options = {}, html_options = {})
-    html_options[:class] ||= "button button_#{type}"
+    html_options[:class] ||= "button_#{type}"
     if html_options[:addclass] then
       html_options[:class] += " " + html_options[:addclass]
       html_options[:addclass] = nil
@@ -15,12 +15,12 @@
   end
   
   def button_remote(button, title, type, url_options = {}, html_options = {})
-    local_html_options = html_options.merge({ :class => "button button_#{type}" })
+    local_html_options = html_options.merge({ :class => "button_#{type}" })
     design_display_button(button, title, url_options, local_html_options, true)
   end
 
   def button_submit_to_remote(button, name, title, options = {}, html_options = {})
-    local_html_options = html_options.merge({ :class => "button button_submit" })
+    local_html_options = html_options.merge({ :class => "button_submit" })
 
     design_display_button_submit_to_remote(button, name,  ( (local_html_options[:class]['with_text'])? title : '' ), options, local_html_options)
   end

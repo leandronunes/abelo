2af30fc3d3bd94d502b2f758ac6261e8159252bf
Leandro Nunes <leandronunes@gmail.com>
2008-01-14 10:58:00 +0000

    ActionItem122: bli

--- db/migrate/045_add_column_sale_items.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnSaleItems < ActiveRecord::Migration
-  def self.up
-    add_column :sale_items, :status, :integer, :default => 0
-  end
-
-  def self.down
-    remove_column :sale_items, :status
-  end
-end

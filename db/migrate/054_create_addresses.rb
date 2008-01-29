d7f28b36998f4a9bf73ebed0fc1f95d873fb7325
Leandro Nunes <leandronunes@gmail.com>
2008-01-29 18:12:40 +0000

    ActionItem141: making the address more flexible

--- db/migrate/054_create_addresses.rb
+++ db/migrate/054_create_addresses.rb
@@ -1,14 +1,14 @@
 class CreateAddresses < ActiveRecord::Migration
   def self.up
     create_table :addresses do |t|
+      t.column :owner_type,           :string, :null => false
+      t.column :owner_id,             :integer, :null => false
       t.column :city,                 :string, :null => false
       t.column :federation_unit,      :string, :null => false
       t.column :street,               :string
       t.column :number,               :string
       t.column :district,             :string
       t.column :zip_code,             :string
-      t.column :phone,                :string
-      t.column :fax,                  :string
     end
   end
 

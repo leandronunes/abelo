a9cf1be931e052286c9bf413b84a1e4b97ecd508
Leandro Nunes <leandronunes@gmail.com>
2008-03-30 15:44:27 +0000

    ActionItem189: adding support for choose the amount o subitems on menu

--- app/design_blocks/menu_website/helpers/menu_website_helper.rb
+++ app/design_blocks/menu_website/helpers/menu_website_helper.rb
@@ -1,13 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module MenuWebsiteHelper
-  def website_menu(menu_itens, html_class = '',  father_path = '')
+  def website_menu(menu_itens, html_class = '',  father_path = '', heigth = 0)
+    heigth = heigth + 1
     content_tag(:ul,
       menu_itens.map do |menu|
+        next if (heigth > @design_block.maximum_subitems)
         link_path =  father_path.blank? ? menu.slug : File.join(father_path,menu.slug)
         [
           content_tag(:li, link_to(menu.title, :page => link_path), :id => "menu_item_#{menu.position}"),
           if menu.has_children?
-            website_menu(menu.children, html_class + ' sub_menu', link_path)
+            website_menu(menu.children, html_class + ' sub_menu', link_path, heigth)
           else
             ''
           end   
--- app/design_blocks/menu_website/models/menu_website.rb
+++ app/design_blocks/menu_website/models/menu_website.rb
@@ -4,4 +4,12 @@ class MenuWebsite < Design::Block
     _("Website's Menu")
   end
 
+  def maximum_subitems= value
+    self.settings[:maximum_subitems] = value.to_i
+  end
+
+  def maximum_subitems
+    self.settings[:maximum_subitems] || 1
+  end
+
 end

d7cec64ce647fe4783e7844a486f8fd15ab218bd
Leandro Nunes dos Santos <leandronunes@gmail.com>
2010-02-04 23:24:43 -0300

    removing getext text

--- app/models/stock_buy.rb
+++ app/models/stock_buy.rb
@@ -27,7 +27,7 @@ class StockBuy < Stock
   end
 
   def validate
-    self.errors.add(:price, _("The price should be at least 0.01" )) if self.price.nil? || self.price <= 0.00
+    self.errors.add(:price, t(:minor_price)) if self.price.nil? || self.price <= 0.00
 
     if !self.validity.nil? and ((self.validity.kind_of?(Time) ? self.validity.to_datetime : self.validity) < (self.date.kind_of?(Time) ? self.date.to_datetime : self.date))
       self.errors.add('validity', '%{fn} is before this entry date. A invalid product cannot enter the stock.')
